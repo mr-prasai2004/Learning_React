765a7ccb465dc020b445b187fb2504fdb8e83e5c
AayushPrasai <gayatriprasai@gmail.com>
2025-01-06 20:04:54 +0545

    Adding Some Logic

--- learning_react/src/App.js
+++ learning_react/src/App.js
@@ -1,18 +1,18 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-import Textform2 from "./components/Textform2";
-import TextForm3 from "./components/TextForm3";
+// import Textform2 from "./components/Textform2";
+// import TextForm3 from "./components/TextForm3";
 function App() {
   return (
     <>
 <Navbar title={"Aayush"}/>
-<div className="container my-3">
+<div className="container my-3 ">
   
 <TextForm heading="Enter your text to analyze below"/>
 </div>
-<Textform2 />
-<TextForm3 />
+{/* <Textform2 />
+<TextForm3 /> */}
     </>
   );
 }
--- learning_react/src/components/TextForm.js
+++ learning_react/src/components/TextForm.js
@@ -13,12 +13,18 @@ export default function TextForm(props) {
     const[text, setText]= useState('Enter Text Here');
     // setText("New Text");
   return (
-    <div>
+    <>
+    <div className='container'>
         <h1>{props.heading}</h1>
 <div className="mb-3">
   <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="10"></textarea>
 </div>
 <button className="btn btn-primary" onClick={handelUpClick}>Convert to Upper Case</button>
 </div>
+<div className="container my-3">
+  <h1>Your Text Summery</h1>
+  <p>{text.split(" ").length} Words, {text.length} Characters</p>
+</div>
+</>
   )
 }
