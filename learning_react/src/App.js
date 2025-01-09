fef90d603c70673139b72ac2971ab410cfa57c0a
AayushPrasai <gayatriprasai@gmail.com>
2025-01-09 21:36:09 +0545

    Adding Auto Dismission Alert Message

--- learning_react/src/App.js
+++ learning_react/src/App.js
@@ -1,39 +1,41 @@
 import "./App.css";
 // import About from "./components/About";
-// import Navbar from "./components/Navbar";
-// import TextForm from "./components/TextForm";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
 // import Testt from "./components/Testt";
 import { useState } from "react";
 import TextFrom from "./components/TextFrom";
+import Alert from "./components/Alert";
 // import TextForm from "./components/TextForm";
 // import Textform2 from "./components/Textform2";
 // import TextForm3 from "./components/TextForm3";
 function App() {
-//   const[mode,setMode]=useState('light');
-//  const  toggleMode = ()=>{
-//     if(mode ==='dark'){
-//     setMode('light')
-//     document.body.style.backgroundColor = 'white'
-//     }
-//     else{
-//       setMode('dark')
-//       document.body.style.backgroundColor = 'gray'
-//     }
+  const[mode,setMode]=useState('light');
+ const  toggleMode = ()=>{
+    if(mode ==='dark'){
+    setMode('light')
+    document.body.style.backgroundColor = 'white'
+    }
+    else{
+      setMode('dark')
+      document.body.style.backgroundColor = 'gray'
+    }
 
     
-  // }
+  }
   return (
     <>
-{/* // <Navbar title='Aayush' mode={mode} toggleMode={toggleMode}/> */}
-{/* // <div className="container my-3 ">
+<Navbar title='Aayush' mode={mode} toggleMode={toggleMode}/>
+<Alert alert="This is a alert message" />
+{/* {/* // <div className="container my-3 "> */}
   
-//  <TextForm heading="Enter your text to analyze below"mode={mode}/> */}
+<TextForm heading="Enter your text to analyze below"mode={mode}/> 
 {/* <About />  */}
 {/* <Testt /> */}
  {/* </div> */}
 {/* <Textform2 />
 <TextForm3 /> */}
-<TextFrom heading="Enter your text to Analysis Below"/>
+{/* <TextFrom heading="Enter your text to Analysis Below"/> */}
     </>
   );
 }
